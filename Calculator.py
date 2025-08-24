b45331e4c757499410d114ad7ea8765f2723416d
Your Name <Panchalom136@gmail.com>
2025-08-24 18:19:44 +0530

    Create Calculator.py

---
+++ Calculator.py
@@ -0,0 +1,10 @@
+# first = 10.1 
+# second = 20
+# sum = 30.1 
+# print (first + second)
+
+
+first = input ("First :")
+second = input ("Second :")
+sum = float(first) + float(second)
+print("Sum:" +str(sum))
