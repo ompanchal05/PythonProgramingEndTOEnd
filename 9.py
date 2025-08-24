3ab3da1e3bb4467e7fcaf9378721cca627c44809
Your Name <Panchalom136@gmail.com>
2025-08-24 18:19:41 +0530

    Create 9.py

---
+++ 9.py
@@ -0,0 +1,10 @@
+#Build in function for the veriabel 
+
+birth_year = input ("Enter your birth year: ")
+age = 2025 - int (birth_year)
+print(age)
+
+#int()
+#float() -> 3.01,3.14,......
+#bool()  -> true , false desicde the desion and like > , <= , >= and more 
+#string () -> Is the function like the Hello world are called as a string 
