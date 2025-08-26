831cfa755c0fb8cfb8bb85d37de419a84a8a15d6
Your Name <Panchalom136@gmail.com>
2025-08-26 21:25:10 +0530

    Create 10.py

---
+++ 10.py
@@ -0,0 +1,14 @@
+#2 Branching 
+
+x= 15
+if x%2 == 0:
+    print("Even")
+elif x%3 == 0:
+    print("Divisible by 3")
+else :
+    print("Odd")
+
+#Ternarry
+age = 20 
+status = "adult" if age >=18 else "Minor"
+print(status) 
