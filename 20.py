2f4ef747af2d652270e2d8d8778bdaabee21be36
Your Name <Panchalom136@gmail.com>
2025-09-01 00:08:15 +0530

    Create 20.py

---
+++ 20.py
@@ -0,0 +1,11 @@
+#function with parameters and without parameters
+def greet():
+    print("Hello, welcome to Python!")
+
+# Calling the function
+greet()
+def greet_user(name):
+    print(f"Hello, {name}! Welcome to Python!")
+
+# Calling the function with an argument
+greet_user("Om")
