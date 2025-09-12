b0d23e1f1ef2dec901759899c0c3324409f260bb
Your Name <Panchalom136@gmail.com>
2025-09-12 05:48:30 +0530

    Create 27.py

---
+++ 27.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+
+# Sample Data
+x = [5, 7, 8, 7, 6, 9, 5, 6]
+y = [99, 86, 87, 88, 100, 86, 103, 87]
+
+# Create Scatter Plot
+plt.scatter(x, y)
+
+# Show Plot
+plt.show()
