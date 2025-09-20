2e9b587f7fac2c52057f44ff790e2a98d6ca4410
Your Name <Panchalom136@gmail.com>
2025-09-20 13:01:46 +0530

    Update 32.py

--- 32.py
+++ 32.py
@@ -1,1 +1,9 @@
-# Create the visiulaztion charts using the matplotlb and scatter librarys in python 
+# Write a regular expression to retrieve all words having 5 character length
+
+import re 
+  
+text = "Hello world, apple mango tiger stone beard happy"
+
+words = re.findall(r'\b\w{5}\b', text)
+
+print ("Words with 5 characters:", words)
