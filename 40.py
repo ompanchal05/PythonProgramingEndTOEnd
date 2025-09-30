a9370aa1d8e7ea67391b8bdb3e4df4453f0cfcda
Your Name <Panchalom136@gmail.com>
2025-09-30 21:01:54 +0530

    Create 40.py

---
+++ 40.py
@@ -0,0 +1,8 @@
+# Program to find ASCII value of a character
+
+char = input("Enter a character: ")
+print("ASCII value of", char, "is", ord(char))
+# Program to find ASCII value of a character
+
+char = input("Enter a character: ")
+print("ASCII value of", char, "is", ord(char))
