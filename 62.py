c5464719c16c7f645ddd0a3c94c11d6efb653dab
Your Name <Panchalom136@gmail.com>
2025-11-02 12:28:30 +0530

    Create 62.py

---
+++ 62.py
@@ -0,0 +1,14 @@
+#WAP FOR THE UNDERSTANDING THE LOGIC OF FOR LOOP AND WHILE LOOP
+
+# For Loop Example
+print("Using for loop:")
+
+for i in range(1, 6):   # runs from 1 to 5
+    print("Iteration", i)
+# While Loop Example
+print("\nUsing while loop:")
+
+i = 1
+while i <= 5:
+    print("Iteration", i)
+    i += 1   # same as i = i + 1
