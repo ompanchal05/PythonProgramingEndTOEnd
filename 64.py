5042a83ec5b93ccacf569a134cefeb71a9847a7d
Your Name <Panchalom136@gmail.com>
2025-11-03 14:48:39 +0530

    Create 64.py

---
+++ 64.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+
+# Data
+x = [1, 2, 3, 4, 5]
+y = [2, 4, 6, 8, 10]
+
+# Plot the data
+plt.plot(x, y)
+
+# Add labels and title
+plt.xlabel("X-axis")
+plt.ylabel("Y-axis")
+plt.title("Simple Line Plot")
+
+# Display the graph
+plt.show()
