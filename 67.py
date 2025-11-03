5b1b1896cb1fc5b85007f1e9414b2545d885bd97
Your Name <Panchalom136@gmail.com>
2025-11-03 21:26:49 +0530

    Create 67.py

---
+++ 67.py
@@ -0,0 +1,14 @@
+# Panda  Library is used to draw shapes and designs
+
+import pandas as pd
+
+data = {
+    'Name' : ['om', 'ana', 'om'],
+    'Age' : [21, 20, 21],
+    'City' : ['surat', 'baroda', 'surat'],
+    'marks' : [85, 90, 85],
+}
+
+df = pd.DataFrame(data)
+
+print(df)
