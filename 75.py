177a57cbb00fa0a50155bc80eb1bb87764e7d218
Your Name <Panchalom136@gmail.com>
2025-11-07 22:59:41 +0530

    Create 75.py

---
+++ 75.py
@@ -0,0 +1,9 @@
+#3. Dictionary of Squares from 1 to 15
+
+# Create dictionary using dictionary comprehension
+
+square_dict = {x: x**2 for x in range(1, 16)}
+
+# Print the dictionary
+print("Dictionary of squares from 1 to 15:")
+print(square_dict)
