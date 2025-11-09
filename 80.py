cadce1dfd3e122d14fd857eef50b0c3c736b330a
Your Name <Panchalom136@gmail.com>
2025-11-09 12:14:59 +0530

    Create 80.py

---
+++ 80.py
@@ -0,0 +1,12 @@
+#type casting and  input () in python 
+
+x = 5 #int -> float
+y = float(x)
+print(y)
+
+a = "10" #str -> int
+b = int(a)
+print(b+5)
+
+name = input("Enter your name:") #Input function 
+print("Hello",name)
