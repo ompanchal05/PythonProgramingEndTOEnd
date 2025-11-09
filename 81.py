71ae348a5c698857959a99e2f748e86a08bbad5b
Your Name <Panchalom136@gmail.com>
2025-11-09 12:15:08 +0530

    Create 81.py

---
+++ 81.py
@@ -0,0 +1,6 @@
+#Task: Write a Python program that takes your name and age as input and prints a greeting.
+
+name = input("Enter Your name:")
+age = input("Enter Your age:")
+
+print("Hello",name + "!" , "You are" ,age , "year old.")
