f8493a5d4e0b554479676c11b2909c8daf7ac7b4
Your Name <Panchalom136@gmail.com>
2025-11-14 14:42:18 +0530

    Create 96.py

---
+++ 96.py
@@ -0,0 +1,11 @@
+#Define a function 
+
+'''' def function_name(parameters):
+    # code
+    return value ''' #syntax of the simple function 
+
+# create the simple function :
+
+def greet():
+    print("Hello , Welcome to python!")
+    greet()
