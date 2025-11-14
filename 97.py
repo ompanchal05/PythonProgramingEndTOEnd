3d4b75e3450dbbbcefb3e5e9c216aa06510d2772
Your Name <Panchalom136@gmail.com>
2025-11-14 21:45:52 +0530

    Create 97.py

---
+++ 97.py
@@ -0,0 +1,7 @@
+#use the rand,randn,randint
+
+import numpy as np
+
+print(np.random.rand(2,2)) #use the rand
+print(np.random.randn(2,2)) #use the randn
+print(np.random.randint(1,10 (2,2))) #use the randint
