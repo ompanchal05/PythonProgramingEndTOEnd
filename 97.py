e82f21b435b4fdb964a96763026645f12f276f19
Your Name <Panchalom136@gmail.com>
2025-11-20 18:30:16 +0530

    Update 97.py

--- 97.py
+++ 97.py
@@ -2,6 +2,6 @@
 
 import numpy as np
 
-print(np.random.rand(2,2)) #use the rand
-print(np.random.randn(2,2)) #use the randn
-print(np.random.randint(1,10 (2,2))) #use the randint
+print(np.random.rand(5)) #use the rand
+print(np.random.randn(5)) #use the randn
+print(np.random.randint(1,10 ,5)) #use the randint
